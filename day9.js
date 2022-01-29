7ee1aea0ca76a94618a88e41552b42c4546b3123
Armoni Tigner <atigner2@student.gsu.edu>
2022-01-28 23:50:02 -0500

    finished part 2 of day 9 assignment for day 10 assignment

--- day9.js
+++ day9.js
@@ -1,26 +1,36 @@
 
+function needsLicense(kind){
+    return kind === 'car'|| kind === 'truck'
+}
+console.log(needsLicense('car'))
 
-function chooseVehicle(option1, option2){
 
 
-/*let toArray = option1.split()
+
+
+/*function chooseVehicle(option1, option2){
+
+
+let toArray = option1.split()
     for (i=0;i < toArray.length;i++){
         console.log(i)
-    } */
+    } 
 
 var toArray = option1;
 for (var i = 0; i < toArray.length; i++) {
-    console.log(toArray.charAt(i));
+    //console.log(toArray.charAt(i));
     
 }
-var toArray1 = option1;
-for (var i = 0; i < toArray.length; i++) {
-    console.log(toArray.charAt(i));
+var toArray1 = option2;
+for (var i = 0; i < toArray1.length; i++) {
+   console.log(toArray1.charAt(i))
+  //return  toArray1.charAt(i)
+} */
+ 
+function chooseVehicle(option1, option2) {
     
-}
+return `${option1 < option2 ? option1 : option2} is clearly the better choice.`;
+    
+ }
 
-
-return toArray[0]
-}
-
-console.log(chooseVehicle('Wuling Hongguang', 'Toyota Corolla'))
+console.log(chooseVehicle('Wuling Hongguang', 'Wuling Gongguang'))
