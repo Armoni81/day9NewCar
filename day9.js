b8b9eb5a2639d84bca73921ba541048339b5f8b4
Armoni Tigner <atigner2@student.gsu.edu>
2022-01-29 13:09:48 -0500

    finished part 3

--- day9.js
+++ day9.js
@@ -1,8 +1,8 @@
 // Part one
-function needsLicense(kind){
+/*function needsLicense(kind){
     return kind === 'car'|| kind === 'truck'
 }
-console.log(needsLicense('car'))
+console.log(needsLicense('car')) */
 
 
 
@@ -37,10 +37,33 @@ for (var i = 0; i < toArray1.length; i++) {
 // THis is the actual soultion to  part two but i dont understand 
 // how it works. What in the code is telling the computer  
 //that A comes before z.
-function chooseVehicle(option1, option2) {
+/*function chooseVehicle(option1, option2) {
     
 return `${option1 < option2 ? option1 : option2} is clearly the better choice.`;
     
  }
 
-console.log(chooseVehicle('Wuling Hongguang', 'Wuling Gongguang'))
+console.log(chooseVehicle('Wuling Hongguang', 'Wuling Gongguang')) */
+
+//Part three 
+// if the car is less than 3 years old it coset 80% of the original price
+//if its more than 10 years old it costs 50% of the orignal price 
+// if its at least 3 years old but not oldet than 10 years it cost 70% of original price
+let nowPrice = 0
+function calculateResellPrice(originalPrice, age){
+
+    if(age < 3){
+     nowPrice +=  originalPrice * 0.80
+     return nowPrice
+    }
+    else if(age > 10){
+        nowPrice += originalPrice * 0.50
+        return nowPrice
+    }
+    else if (age >= 3 || age < 10){
+        nowPrice += originalPrice * 0.70
+        return nowPrice
+    }
+
+}
+console.log(calculateResellPrice(1000,1))
